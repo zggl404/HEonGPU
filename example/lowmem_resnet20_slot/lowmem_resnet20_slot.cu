d5c6f641aeca7dc7b6b4acbe3fccb3ae699ad094
zggl404 <zggl404@nuaa.edu.cn>
2026-01-14 00:46:33 +0800

    plain inference

--- example/lowmem_resnet20_slot/lowmem_resnet20_slot.cu
+++ example/lowmem_resnet20_slot/lowmem_resnet20_slot.cu
@@ -273,6 +273,152 @@ static plainsim::PlainVec layer2_plain(const plainsim::PlainVec& in)
     return res3;
 }
 
+static Ctxt layer2_compare(const Ctxt& in_ct, const plainsim::PlainVec& in_plain)
+{
+    double scaleSx = 0.57;
+    double scaleDx = 0.40;
+    bool timing = verbose > 1;
+
+    Ctxt boot_in_ct = controller.bootstrap(in_ct, timing);
+    plainsim::PlainVec boot_in_plain = plain_controller.bootstrap(in_plain, timing);
+
+    std::vector<Ctxt> res1sx_ct =
+        controller.convbn1632sx(boot_in_ct, 4, 1, scaleSx, timing);
+    std::vector<Ctxt> res1dx_ct =
+        controller.convbn1632dx(boot_in_ct, 4, 1, scaleDx, timing);
+
+    std::vector<plainsim::PlainVec> res1sx_plain =
+        plain_controller.convbn1632sx(boot_in_plain, 4, 1, scaleSx, timing);
+    std::vector<plainsim::PlainVec> res1dx_plain =
+        plain_controller.convbn1632dx(boot_in_plain, 4, 1, scaleDx, timing);
+
+    Ctxt fullpackSx_ct = controller.downsample1024to256(res1sx_ct[0], res1sx_ct[1]);
+    Ctxt fullpackDx_ct = controller.downsample1024to256(res1dx_ct[0], res1dx_ct[1]);
+    plainsim::PlainVec fullpackSx_plain =
+        plain_controller.downsample1024to256(res1sx_plain[0], res1sx_plain[1]);
+    plainsim::PlainVec fullpackDx_plain =
+        plain_controller.downsample1024to256(res1dx_plain[0], res1dx_plain[1]);
+
+    compare_plain_sim_layer("Layer2 after downsample Sx", fullpackSx_plain,
+                            fullpackSx_ct, plain_controller.num_slots);
+    compare_plain_sim_layer("Layer2 after downsample Dx", fullpackDx_plain,
+                            fullpackDx_ct, plain_controller.num_slots);
+
+    controller.num_slots = 8192;
+    plain_controller.num_slots = 8192;
+
+    fullpackSx_ct = controller.bootstrap(fullpackSx_ct, timing);
+    fullpackSx_plain = plain_controller.bootstrap(fullpackSx_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot Sx", fullpackSx_plain,
+                            fullpackSx_ct, plain_controller.num_slots);
+
+    fullpackSx_ct = controller.relu(fullpackSx_ct, scaleSx, timing);
+    fullpackSx_plain = plain_controller.relu(fullpackSx_plain, scaleSx, timing);
+    compare_plain_sim_layer("Layer2 after relu Sx", fullpackSx_plain,
+                            fullpackSx_ct, plain_controller.num_slots);
+
+    fullpackSx_ct = controller.convbn2(fullpackSx_ct, 4, 2, scaleDx, timing);
+    fullpackSx_plain =
+        plain_controller.convbn2(fullpackSx_plain, 4, 2, scaleDx, timing);
+    compare_plain_sim_layer("Layer2 after convbn2 Sx", fullpackSx_plain,
+                            fullpackSx_ct, plain_controller.num_slots);
+
+    Ctxt res1_ct = controller.bootstrap(fullpackDx_ct, timing);
+    plainsim::PlainVec res1_plain = plain_controller.bootstrap(fullpackDx_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot Dx", res1_plain, res1_ct,
+                            plain_controller.num_slots);
+
+    res1_ct = controller.relu(res1_ct, scaleDx, timing);
+    res1_plain = plain_controller.relu(res1_plain, scaleDx, timing);
+    compare_plain_sim_layer("Layer2 after relu Dx", res1_plain, res1_ct,
+                            plain_controller.num_slots);
+
+    res1_ct = controller.add(res1_ct, fullpackSx_ct);
+    res1_plain = plain_controller.add(res1_plain, fullpackSx_plain);
+    compare_plain_sim_layer("Layer2 after add", res1_plain, res1_ct,
+                            plain_controller.num_slots);
+
+    double scale = 0.76;
+    Ctxt res2_ct = controller.convbn2(res1_ct, 5, 1, scale, timing);
+    plainsim::PlainVec res2_plain =
+        plain_controller.convbn2(res1_plain, 5, 1, scale, timing);
+    compare_plain_sim_layer("Layer2 after convbn2(5,1)", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    res2_ct = controller.bootstrap(res2_ct, timing);
+    res2_plain = plain_controller.bootstrap(res2_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot res2", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    res2_ct = controller.relu(res2_ct, scale, timing);
+    res2_plain = plain_controller.relu(res2_plain, scale, timing);
+    compare_plain_sim_layer("Layer2 after relu res2", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    scale = 0.37;
+    res2_ct = controller.convbn2(res2_ct, 5, 2, scale, timing);
+    res2_plain = plain_controller.convbn2(res2_plain, 5, 2, scale, timing);
+    compare_plain_sim_layer("Layer2 after convbn2(5,2)", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    res2_ct = controller.add(res2_ct, controller.mult(res1_ct, scale));
+    res2_plain = plain_controller.add(res2_plain,
+                                      plain_controller.mult(res1_plain, scale));
+    compare_plain_sim_layer("Layer2 after add skip1", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    res2_ct = controller.bootstrap(res2_ct, timing);
+    res2_plain = plain_controller.bootstrap(res2_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot res2b", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    res2_ct = controller.relu(res2_ct, scale, timing);
+    res2_plain = plain_controller.relu(res2_plain, scale, timing);
+    compare_plain_sim_layer("Layer2 after relu res2b", res2_plain, res2_ct,
+                            plain_controller.num_slots);
+
+    scale = 0.63;
+    Ctxt res3_ct = controller.convbn2(res2_ct, 6, 1, scale, timing);
+    plainsim::PlainVec res3_plain =
+        plain_controller.convbn2(res2_plain, 6, 1, scale, timing);
+    compare_plain_sim_layer("Layer2 after convbn2(6,1)", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    res3_ct = controller.bootstrap(res3_ct, timing);
+    res3_plain = plain_controller.bootstrap(res3_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot res3", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    res3_ct = controller.relu(res3_ct, scale, timing);
+    res3_plain = plain_controller.relu(res3_plain, scale, timing);
+    compare_plain_sim_layer("Layer2 after relu res3", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    scale = 0.25;
+    res3_ct = controller.convbn2(res3_ct, 6, 2, scale, timing);
+    res3_plain = plain_controller.convbn2(res3_plain, 6, 2, scale, timing);
+    compare_plain_sim_layer("Layer2 after convbn2(6,2)", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    res3_ct = controller.add(res3_ct, controller.mult(res2_ct, scale));
+    res3_plain = plain_controller.add(res3_plain,
+                                      plain_controller.mult(res2_plain, scale));
+    compare_plain_sim_layer("Layer2 after add skip2", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    res3_ct = controller.bootstrap(res3_ct, timing);
+    res3_plain = plain_controller.bootstrap(res3_plain, timing);
+    compare_plain_sim_layer("Layer2 after boot out", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    res3_ct = controller.relu(res3_ct, scale, timing);
+    res3_plain = plain_controller.relu(res3_plain, scale, timing);
+    compare_plain_sim_layer("Layer2 after relu out", res3_plain, res3_ct,
+                            plain_controller.num_slots);
+
+    return res3_ct;
+}
+
 static plainsim::PlainVec layer3_plain(const plainsim::PlainVec& in)
 {
     double scaleSx = 0.63;
@@ -670,7 +816,9 @@ static void execute_resnet20()
 
     startLayer = utils::start_time();
     controller.print_level_scale(resLayer1, "Layer 2 in");
-    Ctxt resLayer2 = layer2(resLayer1);
+    Ctxt resLayer2 =
+        compare_plain_sim ? layer2_compare(resLayer1, plain_layer1)
+                          : layer2(resLayer1);
     controller.print_level_scale(resLayer2, "Layer 2 out");
     if (debug_plain) {
         debug_decrypt("Layer 2 out", resLayer2, 8);
